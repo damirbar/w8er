e90cc8b0499837ee6c55479f6c3ef442516fe3ff
shay <shayc91@gmail.com>
2018-11-21 15:04:54 +0200

    Open Hourse UI

--- app_android/app/src/main/java/com/w8er/android/activities/OpenHoursActivity.java
+++ app_android/app/src/main/java/com/w8er/android/activities/OpenHoursActivity.java
@@ -1,14 +1,29 @@
 package com.w8er.android.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ScrollView;
+import android.widget.TimePicker;
 
 import com.w8er.android.R;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper;
+
 public class OpenHoursActivity extends AppCompatActivity {
 
+    private final static int TIME_PICKER_INTERVAL = 30;
     private Button mBSave;
+    private TimePicker mTimePicker;
+    private NumberPicker mNumberPicker;
+    private String dayNames[];
 
 
     @Override
@@ -16,6 +31,7 @@ public class OpenHoursActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_hours);
         initViews();
+        initDayPicker();
         getData();
     }
 
@@ -24,7 +40,38 @@ public class OpenHoursActivity extends AppCompatActivity {
         Button mBCancel = findViewById(R.id.cancel_button);
         mBSave.setOnClickListener(view -> saveButton());
         mBCancel.setOnClickListener(view -> finish());
+        mTimePicker = findViewById(R.id.timePicker);
+        mNumberPicker = findViewById(R.id.number_picker);
+
+        setTimePickerInterval(mTimePicker);
+
     }
+
+    private void setTimePickerInterval(TimePicker timePicker) {
+        try {
+            timePicker.setIs24HourView(true);
+            NumberPicker minutePicker = (NumberPicker) timePicker.findViewById(Resources.getSystem().getIdentifier(
+                    "minute", "id", "android"));
+            minutePicker.setMinValue(0);
+            minutePicker.setMaxValue((60 / TIME_PICKER_INTERVAL) - 1);
+            List<String> displayedValues = new ArrayList<String>();
+            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
+                displayedValues.add(String.format("%02d", i));
+            }
+            minutePicker.setDisplayedValues(displayedValues.toArray(new String[0]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initDayPicker() {
+        dayNames = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        mNumberPicker.setMinValue(0);
+        mNumberPicker.setMaxValue(dayNames.length - 1);
+        mNumberPicker.setDisplayedValues(dayNames);
+    }
+
+
 
     private void getData() {
 //        if (getIntent().getExtras() != null) {
