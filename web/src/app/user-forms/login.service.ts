f62c620e75787558e6e35bc5b820911c1d26cd46
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2018-11-26 21:20:49 +0200

    - Posting phone number works in web client

--- web/src/app/user-forms/login.service.ts
+++ web/src/app/user-forms/login.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 import {User} from "../user";
 import { catchError } from 'rxjs/operators';
 import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
+import {Observable} from "rxjs/index";
+import {HttpParamsOptions} from "@angular/common/http/src/params";
 
 
 @Injectable({
@@ -14,14 +16,24 @@ export class LoginService {
 
 
   private handleError: HandleError;
+  private httpOptions = {};
 
 
   constructor(private http: HttpClient, httpErrorHandler: HttpErrorHandler) {
     this.handleError = httpErrorHandler.createHandleError('sService');
+
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'//,
+        // 'Authorization': 'my-auth-token'
+      })
+    };
   }
 
   login(phone_num:string) {
-    return this.http.post<User>('/auth/login-signup', {phone_number: phone_num})
+    console.log("CALLED LOGIN WITH PHONE = " + phone_num);
+    return this.http.post('/auth/login-signup', {phone_number: phone_num},
+      {headers: this.httpOptions})
       .pipe(
         catchError(this.handleError('login', []))
       );
