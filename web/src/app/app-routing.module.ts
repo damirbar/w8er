eee742a73e6bb315e70ce780eab46918a58b789a
Damir <damirbar@gmail.com>
2018-11-27 12:19:14 +0200

    - Temp fix

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RestRequestsComponent } from "./rests/rest-requests/rest-requests.component";
+// import { RestRequestsComponent } from "./rests/rest-requests/rest-requests.component";
 
-const routes: Routes = [
-  {path: 'rest-requests', component: RestRequestsComponent}
-];
+// const routes: Routes = [
+//   {path: 'rest-requests', component: RestRequestsComponent}
+// ];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
