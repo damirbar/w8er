d7eb29b5a62ab4850e1f30776b4e2f0b2c5559d0
shay <shayc91@gmail.com>
2018-11-28 22:43:39 +0200

    Draws on screen

--- app_android/app/src/main/java/com/w8er/android/fragments/MainMapsFragment.java
+++ app_android/app/src/main/java/com/w8er/android/fragments/MainMapsFragment.java
@@ -1,16 +1,20 @@
 package com.w8er.android.fragments;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.gcacace.signaturepad.views.SignaturePad;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,7 +23,6 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.w8er.android.R;
-import com.w8er.android.model.Response;
 import com.w8er.android.model.Restaurant;
 import com.w8er.android.network.RetrofitRequests;
 import com.w8er.android.network.ServerResponse;
@@ -42,11 +45,60 @@ public class MainMapsFragment extends BaseFragment {
     private ServerResponse mServerResponse;
     private CompositeSubscription mSubscriptions;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_maps, container, false);
         mSubscriptions = new CompositeSubscription();
         mServerResponse = new ServerResponse(rootView.findViewById(R.id.layout));
+
+
+
+
+
+
+        SignaturePad mSignaturePad = (SignaturePad) rootView.findViewById(R.id.signature_pad);
+
+        mSignaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+
+            @Override
+            public void onStartSigning() {
+
+                //Event triggered when the pad is touched
+            }
+
+            @Override
+            public void onSigned() {
+                mSignaturePad.clear();
+                //Event triggered when the pad is signed
+            }
+
+            @Override
+            public void onClear() {
+                //Event triggered when the pad is cleared
+            }
+        });
+
+
+
+        mSignaturePad.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int X = (int)event.getX();
+                int Y = (int)event.getY();
+
+//                mServerResponse.showSnackBarMessage("x:" + X + " Y:" + Y );
+
+
+                LatLng L = googleMap.getProjection().fromScreenLocation(new Point(X,Y));
+
+//                GoogleMapUtils.addMapMarker(L,"","",googleMap);
+
+                return false; // Pass on the touch to the map or shadow layer.
+            }
+        });
+
+
 
         initViews(rootView);
         mMapView.onCreate(savedInstanceState);
@@ -77,6 +129,18 @@ public class MainMapsFragment extends BaseFragment {
             if (!initMyLocation(googleMap)) {
                 askPermission();
             }
+
+
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(LatLng point) {
+                    mServerResponse.showSnackBarMessage("Map clicked [" + point.latitude + " / " + point.longitude + "]");
+                    //Do your stuff with LatLng here
+                    //Then pass LatLng to other activity
+                }
+            });
+
+
 
         });
 
@@ -158,7 +222,7 @@ public class MainMapsFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         mMapView.onResume();
-        addToFavoritesProcess(10);
+//        addToFavoritesProcess(10);
     }
 
     @Override
